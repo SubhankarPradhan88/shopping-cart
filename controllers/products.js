cfc0e3205ad1ed795d4f3f1f416f44962d395ef4
subhankar pradhan <subhankarpradhan88@yahoo.com>
2021-05-11 23:23:48 +0530

    Add model and save / read data via file system

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,7 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { 
-        prods: products, 
         pageTitle: 'Add Product', 
         path: '/admin/add-product',
         activeAddProduct: true,
@@ -12,20 +11,23 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title })
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));  // Without any template engine
-    // Render the Shop pug template & passing props to it
-    res.render('shop', { 
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/',
-        hasProducts:  products.length > 0,
-        activeShop: true,
-        productCSS: true
-        // layout: false       // Set this layout key to false so that it disables the default layout, means it won't use the default layout
-    });     
+    Product.fetchAll(products => {
+        // Render the Shop pug template & passing props to it
+        res.render('shop', { 
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/',
+            hasProducts:  products.length > 0,
+            activeShop: true,
+            productCSS: true
+            // layout: false       // Set this layout key to false so that it disables the default layout, means it won't use the default layout
+        }); 
+    });
 }
